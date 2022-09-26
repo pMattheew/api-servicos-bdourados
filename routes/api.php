c60dbfdab7cee0b45e326148b5247f5dbfb5d8cf
Patrick M <patrickmatias.silva@hotmail.com>
2022-09-26 02:58:09 +0000

    serviço de autenticação com sanctum

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ServicoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('servicos', ServicoController::class);
+
+Route::post('/register',[AuthController::class,'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
